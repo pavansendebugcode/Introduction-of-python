3f11e2ab2ccb95553a8cfee3a21157cabd1c911e
pavandebugcode <pavansen202008@gmail.com>
2026-07-05 12:12:00 +0530

    this is version 1 introduction.py

---
+++ introduction.py
@@ -0,0 +1,13 @@
+a = int(input("enter your number"))
+
+if a<18:
+    print("you are not able to vote  ")
+
+    if a<5:
+        print("you are go to school and read books")
+
+else:
+    print("you can do vote ")
+
+
+print("hello world")
