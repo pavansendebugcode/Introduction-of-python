5203eb50a9186eabcbc2e7b1a9bb0a6d6ab6b268
pavandebugcode <pavansen202008@gmail.com>
2026-07-05 17:39:13 +0530

    first commit

---
+++ introduction 2.py
@@ -0,0 +1,17 @@
+a = int(input("enter your number"))
+
+
+b = "this is a version v2"
+
+
+if a<18:
+    print("you are not able to vote  ")
+
+    if a<5:
+        print("you are go to school and read books")
+
+else:
+    print("you can do vote ")
+
+
+print("you are very good person")
---
+++ introduction 3.py
@@ -0,0 +1,20 @@
+a = int(input("enter your number"))
+
+
+b = "this is a version v3"
+
+
+if a<18:
+    print("you are not able to vote  ")
+
+    if a<5:
+        print("you are go to school and read books")
+
+else:
+    print("you can do vote ")
+
+    if a>=20:
+        print("you are genious")
+
+
+print("you are very good person") 
